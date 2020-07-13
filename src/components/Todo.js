48fe6041031eecc20f9abe9d85f00a61014c7c2d
Ajay Sankar <ajay0196@gmail.com>
2020-07-12 20:32:23 -0700

    Added material ui styles to todo list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,22 @@
+import { Checkbox, IconButton, ListItem, Typography } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 import React from 'react';
 
 const Todo = ({todo, deleteTodo, toggleCompleted}) => {
     const {id = 0, task = 'Empty Todo', completed=false} = todo
-    return <li>
-                <div>
-                    <input type="checkbox" id={id} name={task} value={task} checked={completed} onChange={() => toggleCompleted(id)}/>
-                    <label htmlFor={id}> {task}</label>
-                    <input type="button" value="delete" onClick={() => deleteTodo(id)}/>
-                </div>
-            </li>
+    return  <ListItem style={{ display: "flex" }}>
+                <Checkbox checked={completed} onClick={() => toggleCompleted(id)} />
+                <Typography
+                    variant="body1"
+                    style={{
+                    textDecoration: completed ? "line-through" : null
+                    }}
+                >
+                    {task}
+                </Typography>
+                <IconButton onClick={() => deleteTodo(id)}>
+                    <CloseIcon />
+                </IconButton>
+            </ListItem>
 };
 export default Todo;
