4155d456bcfc120f0b38d0ec6b48d25d422a3323
Ajay Sankar <ajay0196@gmail.com>
2020-07-12 18:24:12 -0700

    Added child components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
 
 function App() {
+  const [todos, addTodos] = useState([])
+  const testTodos = [{'id': 1, 'task': 'Study 1', 'completed': true},{'id': 2, 'task': 'Study 2', 'completed': true}];
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World
-        </p>
+        <TodoList todos={testTodos}/>
+        <TodoForm/>
       </header>
     </div>
   );
