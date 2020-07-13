700369f6653bf2b06a38ab58849ba74b1f187c60
Ajay Sankar <ajay0196@gmail.com>
2020-07-12 20:05:08 -0700

    removed unnecessary log

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Todo from './Todo'
 
 const TodoList = ({todos, deleteTodo, toggleCompleted}) => {
-    window.console.log(todos)
     return <ul>
         {
         todos.map((todo = {}) => <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleCompleted={toggleCompleted}/>)
